bc731e5b6a58aa3ace37a47331d1e48d933aab09
730004251 <dawidwas@macbook-pro-dawid.local>
2019-01-19 20:20:55 +0100

    PostDate

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 import Firebase
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
     
    // var ref = Database.database().reference()
     var ref = DatabaseReference.init()
+    var userName:String?
+    
+    @IBOutlet weak var laChatList: UITableView!
     
     
     @IBOutlet weak var txtChatText: UITextField!
@@ -22,7 +33,36 @@ class ViewController: UIViewController {
         
         self.ref = Database.database().reference()
         
+        laChatList.delegate = self
+        laChatList.dataSource = self
+    }
+    
+    var listOfChatInfo = [Chat]()
+    func loadChatRoom(){
         
+        self.ref.child("chat").queryOrdered(byChild: "postDate").observe(.value, with: {
+            (DataSnapshot) in
+            //
+            self.listOfChatInfo.removeAll()
+            
+            if let DataSnapshot = DataSnapshot.children.allObjects as? [DataSnapshot]{
+                
+                for snap in DataSnapshot {
+                    
+                    if let postData = snap.value as? [String:AnyObject]{
+                        let userName = postData["name"] as? String
+                        let text = postData["text"] as? String
+                        
+                        var postDate: CLong
+                        if let postDateIn = postData["postDate"] as? CLong {
+                            postDate = postDateIn
+                        }
+                        
+                    }
+                }
+            }
+            
+        })
     }
 
     func loginAnony(){
